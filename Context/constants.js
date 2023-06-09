260d43a08c1f27acca29b301835cfc9f1266c684
Abhishek <21bcs116@nith.ac.in>
2023-06-10 02:02:48 +0530

    chat-Dapp

---
+++ Context/constants.js
@@ -0,0 +1,6 @@
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+import chatApp from "./ChatApp.json";
+
+export const ChatAppAddress = "0x5F4BF2339cDD0b453ff274E59A3429Ce82E1727a";
+
+export const ChatAppABI = chatApp.abi;
